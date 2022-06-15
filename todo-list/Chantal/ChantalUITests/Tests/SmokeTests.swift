1adadf9201b563ba61bc441ebf1dcdccea9b28a3
e-nadolinskaia <61601568+e-nadolinskaia@users.noreply.github.com>
2022-06-15 22:42:40 +0300

    Add files via upload
    Initial commit

---
+++ todo-list/Chantal/ChantalUITests/Tests/SmokeTests.swift
@@ -0,0 +1,66 @@
+//
+//  AddNewTaskTest.swift
+//  ChantalUITests
+//
+//  Created by Ekaterina Nadolinskaya on 30.05.2022.
+//  Copyright © 2022 Monte Thakkar. All rights reserved.
+//
+
+import XCTest
+
+class SmokeTests: XCTestCase {
+    
+    let newTaskName = MainPage().newTaskName
+    let newTaskTableCell = MainPage().newTaskTableCell
+    
+    
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        XCUIApplication().terminate()
+    }
+    
+    func testAddNewTask() {
+        MainPage()
+            .tapNavigationBarAddButton()
+            .tapAlertTextField()
+            .enterTaskName()
+            .tapAddNewTask()
+    }
+    
+    func testNewTaskExistence() {
+        MainPage()
+            .tapNavigationBarAddButton()
+            .tapAlertTextField()
+            .enterTaskName()
+            .tapAddNewTask()
+            .checkNewTaskAdding()
+    }
+    
+    func testRemoveNewTask() {
+        MainPage()
+            .tapNavigationBarAddButton()
+            .tapAlertTextField()
+            .enterTaskName()
+            .tapAddNewTask()
+            .deletingNewTask()
+    }
+    
+    func testExictenceAfterRemoving() {
+        MainPage()
+        .tapNavigationBarAddButton()
+        .tapAlertTextField()
+        .enterTaskName()
+        .tapAddNewTask()
+        .deletingNewTask()
+        .checkExistenceAfterRemoving(newTaskName).assert("Task not deleted")
+    
+    }
+    
+}
+
